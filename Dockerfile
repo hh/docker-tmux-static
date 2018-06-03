f67e45223699078d1b6c60fb4cf4773a2dfc01ab
Chris McClimans <chris@hippiehacker.org>
2018-06-04 11:02:32 +1200

    some minor updating for tmux1.9

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,30 @@
-from ubuntu:12.10
+#from ubuntu:10.04
+from centos:centos5
 
 maintainer Nate Jones <nate@endot.org>
 
-run apt-get update
-run DEBIAN_FRONTEND=noninteractive apt-get install libevent-dev ncurses-dev build-essential wget -y
+# something in security seems broken
+#Err http://archive.ubuntu.com raring-security/universe amd64 Packages
+#  404  Not Found [IP: 91.189.88.149 80]
+
+#run sed -i 's/\(deb.*securit\)/##\ \1/'  /etc/apt/sources.list
+
+#run apt-get update
+
+# Don't install 32bit
+run echo multilib_policy=best >> /etc/yum.conf
+run echo 'exclude="kernel*"' >> /etc/yum.conf
+# maybe don't run update? Grab oldest stuff possible?
+# updates to centos-release-5-11, maybe just leave at defaults
+# run yum update -y
+run yum groupinstall -y "Development Tools"
+run yum install -y ncurses-devel curl wget
+
+# run DEBIAN_FRONTEND=noninteractive apt-get install ncurses-dev build-essential wget -y
+# run DEBIAN_FRONTEND=noninteractive apt-get install curl -y
+#run
+# error: 'EVBUFFER_EOL_LF' undeclared
+# libevent-dev
 run mkdir /tmux
 
 add build.sh /tmux/build.sh
--- build.sh
+++ build.sh
@@ -3,13 +3,36 @@
 set -e
 set -x
 
-VERSION=1.8
+# Wanting to build a static tmux binary that works on Centos 5
 
-wget -O tmux-$VERSION.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-$VERSION/tmux-$VERSION.tar.gz/download
+export VERSION=1.9a
+export LDFLAGS="-L/usr/lib64:/tmux/lib -static -static-libgcc"
+export LIBS="-ltinfo"
+export INCLUDES="-I/tmux/include"
+
+# Help configure find libevent info
+export PKG_CONFIG_PATH=/tmux/lib/pkgconfig
+
+# Only needed on Ubuntu 10.04 and earler as libncurses still probvides libtinfo at this point in history
+ln -s /usr/lib/libncurses.a /usr/lib/libtinfo.a
+# We are building on multiarch, 64bit is in another path
+ln -s /usr/lib64/libncurses.a /usr/lib64/libtinfo.a
+
+## libevent is only needed on ubuntu 10.04
+# curl is needed for github, it's got funky
+wget http://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz/download
+tar xvfz libevent-2.0.21-stable.tar.gz
+
+cd  libevent-2.0.21-stable
+./configure --enable-static --prefix=/tmux
+make install
+cd ..
+
+# Part of me wants a git build
+wget -O tmux-$VERSION.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-1.9/tmux-$VERSION.tar.gz/download
 tar xzvf tmux-$VERSION.tar.gz
+# TMUX 1.9a isn't in the tmux-1.9a folder... manually changing
 cd tmux-$VERSION
-
-LDFLAGS="-static -static-libgcc" LIBS="-ltinfo" ./configure --enable-static
-LDFLAGS="-static -static-libgcc" make
-
+./configure --enable-static
+make
 cp tmux ..
